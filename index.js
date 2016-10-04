7d88642e55b6bbb68aba7279d12135a7c5912ebd
lasalvavida <rht25@drexel.edu>
2016-10-04 13:14:37 -0400

    Fixed module exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Colorspace : require('./lib/Colorspace'),
   Image : require('./lib/Image'),
-  Kernel : require('./lib/Matrix2d')
+  Kernel : require('./lib/Kernel'),
+  Matrix2d : require('./lib/Matrix2d')
 };
