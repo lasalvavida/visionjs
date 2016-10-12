2c99b033c3ab9fb838daa683d8e563d6ba2ebfcb
lasalvavida <rht25@drexel.edu>
2016-10-11 22:01:58 -0400

    Export SURF

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   Colorspace : require('./lib/Colorspace'),
   Image : require('./lib/Image'),
   Kernel : require('./lib/Kernel'),
-  Matrix2d : require('./lib/Matrix2d')
+  Matrix2d : require('./lib/Matrix2d'),
+  SURF : requrie('./libSURF')
 };
